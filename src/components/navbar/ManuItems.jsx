587f18b778135474af3951b5b024b0d12a0077b1
shoumik-123 <shabikyeaminshoumik123@gmail.com>
2023-06-13 19:35:53 +0600

    first commit

---
+++ src/components/navbar/ManuItems.jsx
@@ -0,0 +1,28 @@
+export const ManuItems = [
+    {
+        title:"Home",
+        uri:"#",
+        className:"nav-link"
+    },
+    {
+        title:"Services",
+        uri:"#",
+        className:"nav-link"
+    },
+    {
+        title:"Products",
+        uri:"#",
+        className:"nav-link"
+    },
+    {
+        title:"Contact Us",
+        uri:"#",
+        className:"nav-link"
+    },
+    {
+        title:"Sign Up",
+        uri:"#",
+        className:"nav-link-mobile"
+    },
+
+]
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { ManuItems } from './ManuItems';
+import '../../assets/css/navbar.css';
+
+const Navbar = () => {
+    const [clicked, setClicked] = useState(false);
+
+    const handleClick = () => {
+        setClicked(!clicked);
+    };
+    return (
+        <div>
+            <nav className="NavbarItems">
+                <h1 className="nav-logo">
+                    Logo<i className="fab fa-react"></i>
+                </h1>
+                <div className="menu-icon" onClick={handleClick}>
+                    <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                <ul>
+                    {ManuItems.map((item, i) => {
+                        return (
+                            <li key={i}>
+                                <a className={item.className} href={item.uri}>
+                                    {item.title}
+                                </a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
